feeec0c54cded6d3f6670f9669cf7e738856a3b9
AlterEgo404 <Hoanglongcntt910@gmail.com>
2025-10-11 08:45:37 +0000

    1.4.13

--- main.py
+++ main.py
@@ -1423,16 +1423,11 @@ async def stats(ctx, member: discord.Member = None):
     resistance = round(tf['resistance'] * 100, 1)
 
     equips = tf.get("equips", [])
-    equip_line = " | ".join(
-        _item_display(equips[i]) if i < len(equips) and equips[i] else "  :black_large_square:  "
-        for i in range(3)
-    )
 
     # --- Chuỗi hiển thị chính ---
     stats_text = (
         f"**HP:** {hp} <:Health:1426153576249430079>\n"
         f"**Mana:** {mana} <:Mana:1426153608361279558>\n"
-        f"{equip_line}\n\n"
     )
 
     # --- Embed hiển thị ---
@@ -1441,18 +1436,31 @@ async def stats(ctx, member: discord.Member = None):
         description=stats_text,
         color=discord.Color.red()
     )
+
+    for i in range(3):
+        item = equips[i] if i < len(equips) and equips[i] else None
+        embed.add_field(
+            name=f"",
+            value=_item_display(item) if item else ":black_large_square:",
+            inline=True
+        )
+    embed.add_field(name="", value="", inline=False)
+
     embed.add_field(name = f"<:AD:1426154602335698974>", value = f"{ad}", inline = True)
     embed.add_field(name = f"<:AP:1426153499766427679>", value = f"{ap}", inline = True)
     embed.add_field(name = f"<:scalemanaregen:1426483869120594070>", value = f"0", inline = True)
     embed.add_field(name="", value="", inline=False)
+
     embed.add_field(name = f"<:Armor:1426153517609127966>", value = f"{armor}", inline = True)
     embed.add_field(name = f"<:MagicResist:1426153593148411934>", value = f"{magic_resist}", inline = True)
     embed.add_field(name = f"<:AS:1426153532620279951>", value = f"{attack_speed}", inline = True)
     embed.add_field(name="", value="", inline=False)
+
     embed.add_field(name = f"<:CritChance:1426153545131884617>", value = f"{crit_rate}%", inline = True)
     embed.add_field(name = f"<:CritDamage:1426153557798944849>", value = f"{crit_damage}%", inline = True)
     embed.add_field(name = f"<:scaleSV:1426154642676646039>", value = f"{lifesteal}%", inline = True)
     embed.add_field(name="", value="", inline=False)
+    
     embed.add_field(name = f"<:scaleDA:1426153627281526886>", value = f"{amplify}%", inline = True)
     embed.add_field(name = f"<:scaleDR:1426153642527817799>", value = f"{resistance}%", inline = True)
 
